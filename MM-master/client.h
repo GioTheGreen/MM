1258d4515047119be094ad859e91b89cd6d59ced
GioTheGreen <67901475+GioTheGreen@users.noreply.github.com>
2023-05-04 12:55:42 +0100

    Add files via upload

---
+++ MM-master/client.h
@@ -0,0 +1,14 @@
+#ifndef CLIENT_H_INCLUDED
+#define CLIENT_H_INCLUDED
+
+
+class client {
+public:
+    int runClient();
+    bool findServer();
+    void createServer();
+    void sendMessage();
+};
+
+
+#endif // CLIENT_H_INCLUDED
---
+++ MM-master/server.h
@@ -0,0 +1,9 @@
+#ifndef SERVER_H_INCLUDED
+#define SERVER_H_INCLUDED
+
+class server {
+public:
+    int runServer();
+};
+
+#endif // SERVER_H_INCLUDED
